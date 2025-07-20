15fb88b20b4f774218d498fd163f75ad182f82b3
tailison-ramos <tailisonramos427@gmail.com>
2025-07-20 00:17:41 -0300

    first commit

---
+++ projetoMario/src/index.js
@@ -0,0 +1,180 @@
+const player1 = {
+  name: "Mario",
+  velocity: 4,
+  handling: 3,
+  power: 3,
+  score: 0
+}
+
+const player2 = {
+  name: "Luigi",
+  velocity: 3,
+  handling: 4,
+  power: 4,
+  score: 0
+}
+
+
+
+async function rollDice() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+
+async function getRandomBlock() {
+
+  let random = Math.random()
+  let randomBlock
+
+  switch (true) {
+    case random < 0.33:
+      randomBlock = "RETA"
+      break;
+
+    case random < 0.66:
+      randomBlock = "CURVA"
+      break;
+
+
+    default:
+      randomBlock = "CONFRONTO"
+      break;
+  }
+
+  return randomBlock
+}
+
+
+async function playRaceEngine(character1, character2) {
+
+  // Definindo a quantidade de rounds
+  let maxRound = 5
+
+  for (let round = 1; round <= maxRound; round++) {
+
+    console.log(`\n⌚>> Rodada ${round} `)
+
+    let block = await getRandomBlock()
+    console.log(` Bloco -> ${block}`)
+
+
+    // Rolar os dados
+    let diceResult1 = await rollDice()
+    let diceResult2 = await rollDice()
+
+
+    // Teste habilidade
+    let totalTestSkill1 = 0
+    let totalTestSkill2 = 0
+
+    if (block === "RETA") {
+      totalTestSkill1 = diceResult1 + character1.velocity
+      totalTestSkill2 = diceResult2 + character2.velocity
+
+      await logRollResult(character1.name, "Velocidade->", diceResult1, character1.velocity)
+      await logRollResult(character2.name, "Velocidade->", diceResult2, character2.velocity)
+    }
+
+    if (block === "CURVA") {
+      totalTestSkill1 = diceResult1 + character1.handling
+      totalTestSkill2 = diceResult2 + character2.handling
+
+      await logRollResult(character1.name, "Manobrabilidade->", diceResult1, character1.handling)
+      await logRollResult(character2.name, "Manobrabilidade->", diceResult2, character2.handling)
+    }
+
+    if (block === "CONFRONTO") {
+      let powerResult1 = diceResult1 + character1.power;
+      let powerResult2 = diceResult2 + character2.power;
+
+      console.log(`${character1.name} confrontou com ${character2.name}! 🥊`);
+
+      await logRollResult(
+        character1.name,
+        "Poder",
+        diceResult1,
+        character1.power
+      );
+
+      await logRollResult(
+        character2.name,
+        "Poder",
+        diceResult2,
+        character2.power
+      );
+
+      if (powerResult1 > powerResult2 && character2.score > 0) {
+        console.log(
+          `${character1.name} venceu o confronto! ${character2.name} perdeu 1 ponto 🐢`
+        );
+        character2.score--;
+      }
+
+      if (powerResult2 > powerResult1 && character1.score > 0) {
+        console.log(
+          `${character2.name} venceu o confronto! ${character1.name} perdeu 1 ponto 🐢`
+        );
+        character1.score--;
+      }
+
+      console.log(
+        powerResult2 === powerResult1
+          ? "Confronto empatado! Nenhum ponto foi perdido"
+          : ""
+      );
+    }
+
+
+    if (totalTestSkill1 > totalTestSkill2) {
+      console.log(`${character1.name} marcou 1 ponto!`)
+      character1.score++
+    }
+
+    else if (totalTestSkill2 > totalTestSkill1) {
+      console.log(`${character2.name} marcou 1 ponto!`)
+      character2.score++
+    }
+    // else (
+    //   console.log("Ninguém marcou pontos")
+    // )
+  }
+
+}
+
+
+async function logRollResult(characterName, block, diceResult, attribute) {
+  console.log(`${characterName} 🎲 rolou um dado de ${block} ${diceResult} + ${attribute} = ${diceResult + attribute}`)
+}
+
+
+async function declareWinner(character1, character2) {
+  console.log("\n ======= Resultado Final =========")
+
+  console.log(`>> ${character1.name}: ${character1.score} ponto(s)`)
+  console.log(`>> ${character2.name}: ${character2.score} ponto(s)`)
+
+  if (character1.score > character2.score) {
+    console.log(``)
+    console.log(`${character1.name} é o campeão!🚀`)
+  }
+
+  else if (character2.score > character1.score) {
+    console.log(`${character2.name} é o campeão!🚀`)
+  }
+
+  else {
+    console.log("Corrida terminou em empate")
+  }
+}
+
+// Função main utilizando o auto invoke ()=>{}()
+(
+  async function main() {
+    console.log("🚨Vamos correr!🚨 \n")
+    console.log(`🏁 ${player1.name} x ${player2.name} \n`)
+
+    await playRaceEngine(player1, player2)
+
+    await declareWinner(player1, player2)
+  })();
+
