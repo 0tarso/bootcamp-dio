c4042299d3dc25f9c584725b04dcf7932b5da893
tailison-ramos <tailisonramos427@gmail.com>
2025-07-22 23:08:54 -0300

    add: aula de comandos npm

---
+++ aulas/07-npm-packages-basic/src/main.js
@@ -0,0 +1,24 @@
+//Alguns comandos npm
+
+// npm init -y
+// inicia um novo projeto Node.js com um package.json padrão
+
+// npm install <nome-do-pacote>
+// instala um pacote específico e adiciona ao package.json
+
+// npm install <nome-do-pacote>@<versão>
+// instala uma versão específica de um pacote
+
+// npm install <nome-do-pacote> --save-dev
+// instala um pacote como dependência de desenvolvimento
+
+// npm uninstall <nome-do-pacote>
+// desinstala um pacote e remove do package.json
+
+// npm update
+// atualiza todos os pacotes para a versão mais recente
+
+import chalk from 'chalk';
+
+console.log(chalk.blue.bgGray("Hello, World!"));
+console.log(chalk.green("This is a simple Node.js application."));
